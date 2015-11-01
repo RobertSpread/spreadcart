14b5bb54fc3708223c078c5b36305d51f305c302
RobertSpread <seete6ohS>
2015-11-01 20:54:19 +0100

    initial styling

--- spreadCart.js
+++ spreadCart.js
@@ -3,6 +3,8 @@ tld="de"; //.com,.de,.co.uk or any other supported domain
 shopID="0";//your shop id
 mediaURL="//image.spreadshirtmedia.net/image-server/v1/products/";
 returnURL= encodeURIComponent(window.location.href);// location from where the customer enters checkout
+totalQuantity=0;
+updateCycle=0;
 
 jQuery( document ).ready(function() {
     buildCustomMiniBasket();
@@ -13,25 +15,32 @@ jQuery( document ).ready(function() {
     }
 
     function buildCustomMiniBasket(){
-        totalQuantity=0;
-
         basketData=getBasketData();
-        jQuery('#myBasket').empty();
-        jQuery('#myBasket').append('<div id="miniBasket" class="miniBasketButton  viewBasketDetails">Show Basket</div><div id="miniBasketDetails" style="display:none"></div>');
+        jQuery('#miniBasket').remove();
+        jQuery('#'+basketContainer).append('<div id="miniBasket" class="miniBasketButton  viewBasketDetails fa fa-shopping-cart"> Show Basket</div>');
+        jQuery('#'+basketContainer).append('<div id="miniBasketDetails" style="display:none"></div>');
         jQuery.each( basketData.orderListItems, function(index ){
-            jQuery('#miniBasketDetails').append('<img src="'+mediaURL+basketData.orderListItems[index].productId+'"/>quantity:'+basketData.orderListItems[index].quantity+' size:'+basketData.orderListItems[index].sizeName+' name:'+basketData.orderListItems[index].ptName)
+            jQuery('#miniBasketDetails').append('<div class="basketItem" id="basketItem-'+index+'"></div>');
+            jQuery('#basketItem-'+index).append('<img src="'+mediaURL+basketData.orderListItems[index].productId+'"/>quantity:'+basketData.orderListItems[index].quantity+' size:'+basketData.orderListItems[index].sizeName+' name:'+basketData.orderListItems[index].ptName+' price:'+basketData.orderListItems[index].price)
             totalQuantity+=basketData.orderListItems[index].quantity;
         });
-        //jQuery('#miniBasket').append('<div id="totalQuantity">'+totalQuantity+'</div>');
+        jQuery('#miniBasketDetails').append('<div class="total" id="">Total:'+basketData.priceTotal+'</div>');
+        jQuery('#miniBasket').html('<div id="totalQuantity">'+totalQuantity+'</div>');
+        jQuery('#miniBasketDetails').append('<button class="miniBasketButton" id="continueShoppingLink">Continue Shopping</button>');
         jQuery('#miniBasketDetails').append('<button class="miniBasketButton" id="checkoutLink">Go to Checkout</button>');
         jQuery('#checkoutLink').on("click",function(){window.location="https://checkout.spreadshirt."+tld+"/?basketId="+basketData.apiBasketId+"&shopId="+shopID+"&emptyBasketUrl="+returnURL});
         jQuery('#miniBasket').on("click",function(){showMiniBasket()});
+        jQuery('#continueShoppingLink').on("click",function(){showMiniBasket()});
+
     }
 
     function showMiniBasket(){
         $( "#miniBasketDetails" ).toggle( )
     }
 
+    function updateMiniBasket(){
+        buildCustomMiniBasket()
+    }
 });
 
 
